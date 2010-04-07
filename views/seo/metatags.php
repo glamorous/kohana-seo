c65b465c48181c5e4daafdcdd4fd3169067444e7
Jonas De Smet <jonas@glamorous.be>
2010-04-07 23:53:02 +0200

    Basic view

---
+++ views/seo/metatags.php
@@ -0,0 +1,4 @@
+<!-- META TAGS FROM SEO MODULE -->
+<meta name="robots" content="<?php echo $seo->robots; ?>" />
+<meta name="keywords" content="<?php echo $seo->keywords; ?>" />
+<meta name="description" content="<?php echo $seo->description; ?>" />
